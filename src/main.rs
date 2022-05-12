4ac51d57baba28ece20f694aeea73bfc049c463b
tesilio <lodcoco@gmail.com>
2022-05-12 23:14:50 +0900

    First commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+// const ONE: &str = "ONE";
+// const TWO: &str = "TWO";
+// const ONE_AND_TWO: [&str; 2] = [ONE, TWO];
+
+fn main() {
+  let words = String::from("hello world");
+  println!("{}", words);
+  println!("{}", "ghi");
+}
